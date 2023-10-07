975b4269dcc37e33dbad721e2a6a3e0ebb5ede2f
Younrag <younes.raguebi@usmba.ac.ma>
2023-10-07 22:13:16 +0100

    Done

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,3 +3,5 @@ def replace_in_list(my_list, idx, element):
     if idx in range(0, len(my_list)):
         del my_list[idx]
         my_list.insert(idx, element)
+        return my_list
+    return my_list
