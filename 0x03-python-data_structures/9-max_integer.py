6f53109482e316613e295834c364eda221bbaccd
Younrag <younes.raguebi@usmba.ac.ma>
2023-10-08 20:56:38 +0100

    Done

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    l = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i - 1] < my_list[i]:
+            l = my_list[i]
+    return l
