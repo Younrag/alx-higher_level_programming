6707dca19e99d410668502441a80c71d219e2564
Younrag <younes.raguebi@usmba.ac.ma>
2023-10-08 20:58:20 +0100

    Done

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     l = my_list[0]
     for i in range(1, len(my_list)):
-        if my_list[i - 1] < my_list[i]:
+        if l < my_list[i]:
             l = my_list[i]
     return l
