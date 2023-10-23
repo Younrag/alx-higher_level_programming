214aaa1b5eed2bc6c7bb0dad2b5aad92a1f8e1dc
Younrag <younes.raguebi@usmba.ac.ma>
2023-10-23 23:19:42 +0100

    Done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     n = 0
     try:
-        for i in range(x):
+        while i is not x:
             print(my_list[i], end="")
             n += 1
     except IndexError:
