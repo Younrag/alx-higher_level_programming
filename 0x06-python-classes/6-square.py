d2974234c6eb8021c4bd3a34fa418258319948e8
Younrag <younes.raguebi@usmba.ac.ma>
2023-10-24 16:32:02 +0100

    Donee

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,7 +43,7 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        [print("") for i in range(0, self.__position[1])]
+        [print("") for l in range(0, self.__position[1])]
         for i in range(0, self.__size):
             for j in range(self.__position[0]):
                 print(" ", end="")
