f449537f761bcef962a9ba366db40d0bb65349f0
Younrag <younes.raguebi@usmba.ac.ma>
2023-11-07 00:19:50 +0100

    Done

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""Class MyInt"""
 
 
 class MyInt (int):
+    """!= to =="""
     def __ev__(self, other):
         return int(self) != other
 
     def __pk__(self, other):
+        """== to !="""
         return int(self) == other
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Defines a function that adds attributes to objects."""
 
 
 def add_attribute(obj, name, value):
+    """Add a new attribute to an object if possible.
+    Args:
+        obj: The object to add an attribute to.
+        att: The name of the attribute to add to obj.
+        value: The value of att.
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
     if hasattr(obj, '__dict__') or name in getattr(obj, '__slots__', {}):
         setattr(obj, name, value)
     else:
