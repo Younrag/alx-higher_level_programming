493b040cca456372b1b7f6bd7ce1b999de603d78
Younrag <younes.raguebi@usmba.ac.ma>
2023-11-06 23:53:29 +0100

    Done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Module for lookup"""
 
 
 def lookup(obj):
+    """Looks up object.
+    Args:
+        obj: the object to list.
+
+    Returns:
+        list: list of attributes.
+    """
     return dir(obj)
