85987ebaba8240ee68ff797373f8b64a8e2062d8
Younrag <younes.raguebi@usmba.ac.ma>
2023-11-08 20:25:06 +0100

    Done

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
+def facto(k):
+    fact = 1
+    for i in range(1, k):
+        fact = fact * (i + 1)
+    return fact
 
+def pascal_triangle(n):
+    L = []
+    if n <= 0:
+        return L
+    else:
+        for i in range(n):
+            row = []
+            for j in range(i):
+                c = facto(i) / (facto(j) * facto(i - j))
+                row.append(int(c))
+            L.append(row)
+        return L
