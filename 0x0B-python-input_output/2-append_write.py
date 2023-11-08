5e4a9f6bb55f39bf6a69c9e4d1b4dfbbd95338f5
Younrag <younes.raguebi@usmba.ac.ma>
2023-11-08 20:48:13 +0100

    Done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Modul"""
 
 
 def append_write(filename="", text=""):
+    """write append"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
