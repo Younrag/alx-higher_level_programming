439f6e8e90fa6221c16f852a3d6ed8f8fc560a48
Younrag <younes.raguebi@usmba.ac.ma>
2023-11-08 20:51:24 +0100

    Done

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Modul"""
 
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """json"""
     with open(filename, "w", encoding="UTF-8") as f:
         json.dump(my_obj, f)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Modul"""
 
 
 import json
 
 
 def load_from_json_file(filename):
+    """json"""
     with open(filename, "r", encoding="UTF-8") as f:
         return json.load(f)
