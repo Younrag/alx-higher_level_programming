4d7c6145bb1fa5825275b20e74e739ac5928339c
Younrag <younes.raguebi@usmba.ac.ma>
2024-01-16 21:04:34 +0100

    Done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+const no = 'No argument';
+const yes = 'Arguments found';
+var count = 0;
+// print process.argv
+argv.forEach((val, index) => {
+	count++;
+});
+if (count == 2){
+	console.log(no);
+}
+else if(count >= 3) {
+	console.log(yes);
+}
