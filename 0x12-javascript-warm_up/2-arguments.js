52011b7ffd0570cd40b7251b4ae1594357affb9e
Younrag <younes.raguebi@usmba.ac.ma>
2024-01-16 21:16:58 +0100

    Done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,13 @@
 
 const process = require('process');
 const no = 'No argument';
-const yes = 'Arguments found';
+const yes = 'Argument found';
+const yes2 = 'Arguments found';
 // print process.argv
 if (process.argv.length === 2) {
   console.log(no);
-} else if (process.argv.length >= 3) {
+} else if (process.argv.length === 3) {
   console.log(yes);
+} else {
+  console.log(yes2);
 }
