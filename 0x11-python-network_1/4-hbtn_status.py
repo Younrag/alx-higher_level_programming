43ed6723733e0dae30b6533f9150c4b6a9d710bf
Younrag <younes.raguebi@usmba.ac.ma>
2024-01-31 01:17:49 +0100

    Done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 
 
 
-f __name__ == "__main__":
+if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
     t = r.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
